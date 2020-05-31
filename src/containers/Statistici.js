f8706861e10c97d60de195f5747e0b2bcef96006
Bodin Roxana <bodinroxana@yahoo.com>
2020-05-31 23:38:52 +0300

    ga analytics reports

--- src/containers/Statistici.js
+++ src/containers/Statistici.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import GoogleLogin from 'react-google-login';
 var VIEW_ID = '219697764';
-var METRIC='ga:totalEvents';//users//ga:pagePath
 
 class Statistici extends Component {
   queryReports() {
@@ -32,7 +31,7 @@ class Statistici extends Component {
       
       //var formattedJson = JSON.stringify(response.result.reports[0].data.rows[0].metrics[0], null, 2);
       
-      document.getElementById('query-output').value = response.result.reports[0].data.rows[0].metrics[0].values;
+      document.getElementById('query-output').value = response.result.reports[0].data.totals[0].values;
 }); 
     }
   render(){
@@ -42,10 +41,11 @@ class Statistici extends Component {
     return (
         <body>
         <h1>Hello Analytics Reporting API V4</h1>
+
         <GoogleLogin
               clientId="443094691967-2j7a99kuh7puj3dvb7m7f9i40j6lcjr3.apps.googleusercontent.com"
               buttonText="Continuă cu GOOGLE"
-              onSuccess={this.queryReports()}
+              onSuccess={this.queryReports}
               onFailure={responseGoogleFailure}
             />
 
