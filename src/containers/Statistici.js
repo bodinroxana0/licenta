1560c66072d3134d827932f88a0feaa71dedb18e
Bodin Roxana <bodinroxana@yahoo.com>
2020-05-31 21:09:02 +0300

    ga analytics reports

--- src/containers/Statistici.js
+++ src/containers/Statistici.js
@@ -6,13 +6,6 @@ class Statistici extends Component {
       super(props);    
     }
     
-    componentDidMount(){
-      window.gapi.auth2.init({
-        client_id: "443094691967-2j7a99kuh7puj3dvb7m7f9i40j6lcjr3.apps.googleusercontent.com", //paste your client ID here
-        scope: "https://www.googleapis.com/auth/analytics.readonly",
-      });
-       
-  }
   render(){
     const queryReport = () => {//(1)
       window.gapi.client
