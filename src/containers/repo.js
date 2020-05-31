52cee4039391873175365c9ccf2ee6ce57192f9c
Bodin Roxana <bodinroxana@yahoo.com>
2020-05-31 20:52:55 +0300

    ga analytics reports

--- src/containers/repo.js
+++ src/containers/repo.js
@@ -1,9 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 var VIEW_ID = '219697764';
-const repo = () => {
-  const [data, setData] = useState([]);
 
-  useEffect(() => {
+class Repo extends Component {
+    constructor(props) {
+      super(props);    
+    }
+    
+    componentDidMount(){
+      window.gapi.auth2.init({
+        client_id: "443094691967-2j7a99kuh7puj3dvb7m7f9i40j6lcjr3.apps.googleusercontent.com", //paste your client ID here
+        scope: "https://www.googleapis.com/auth/analytics.readonly",
+      });
+       
+  }
+  render(){
     const queryReport = () => {//(1)
       window.gapi.client
         .request({
@@ -36,9 +46,9 @@ const repo = () => {
         })
         .then(displayResults, console.error.bind(console));
     };
-
-    const displayResults = (response) => {//(2)
+   const displayResults = (response) => {//(2)
       const queryResult = response.result.reports[0].data.rows;
+      console.log(queryResult);
       const result = queryResult.map((row) => {
         const dateSting = row.dimensions[0];
         const formattedDate = `${dateSting.substring(0, 4)}
@@ -48,15 +58,11 @@ const repo = () => {
           visits: row.metrics[0].values[0],
         };
       });
-      setData(result);
     };
-
-    queryReport();
-  }, []);
-
-  return data.map((row) => (
-    <div key={row.date}>{`${row.date}: ${row.visits} visits`}</div>//(3)
-  ));
-};
-
-export default repo;
+    return (
+      <div>
+        <button onClick={queryReport}>Click me</button>
+      </div>
+    )}
+}
+export default Repo;
