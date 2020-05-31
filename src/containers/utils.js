4070a1e5892a3de2d1bacb6c347fd64b45722976
Bodin Roxana <bodinroxana@yahoo.com>
2020-05-31 20:10:21 +0300

    ga analytics reports

--- src/containers/utils.js
+++
@@ -1,6 +0,0 @@
-const initAuth = () => {
-    return window.gapi.auth2.init({
-      client_id: "443094691967-2j7a99kuh7puj3dvb7m7f9i40j6lcjr3.apps.googleusercontent.com", //paste your client ID here
-      scope: "https://www.googleapis.com/auth/analytics.readonly",
-    });
-  };
