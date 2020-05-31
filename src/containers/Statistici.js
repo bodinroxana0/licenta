8eee79f6ce1559a90cdb1719cf2b39bdbf72e70d
Bodin Roxana <bodinroxana@yahoo.com>
2020-05-31 20:08:55 +0300

    ga analytics reports

--- src/containers/Statistici.js
+++ src/containers/Statistici.js
@@ -9,6 +9,10 @@ const Report = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
+    window.gapi.auth2.init({
+      client_id: "443094691967-2j7a99kuh7puj3dvb7m7f9i40j6lcjr3.apps.googleusercontent.com", //paste your client ID here
+      scope: "https://www.googleapis.com/auth/analytics.readonly",
+    });
     const queryReport = () => {//(1)
       window.gapi.client
         .request({
@@ -64,4 +68,4 @@ const Report = () => {
   ));
 };
   
-export default Statistici;
+export default Report;
