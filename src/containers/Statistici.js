1b927e91dad22b09dc96af72d0e1d84de8890ae9
Bodin Roxana <bodinroxana@yahoo.com>
2020-05-31 21:34:14 +0300

    ga analytics reports

--- src/containers/Statistici.js
+++ src/containers/Statistici.js
@@ -1,18 +1,11 @@
 import React, { Component } from "react";
+import GoogleLogin from 'react-google-login';
 var VIEW_ID = '219697764';
 
 class Statistici extends Component {
-    constructor(props) {
-      super(props);    
-    }
-    displayResults(response) {
-      console.log("aici");
-      var formattedJson = JSON.stringify(response.result, null, 2);
-      console.log(formattedJson);
-      document.getElementById('query-output').value = formattedJson;
-    }
-     // Query the API and print the results to the page.
-   queryReports() {
+  render(){
+         // Query the API and print the results to the page.
+  function queryReports() {
     window.gapi.client.request({
       path: '/v4/reports:batchGet',
       root: 'https://analyticsreporting.googleapis.com/',
@@ -35,16 +28,27 @@ class Statistici extends Component {
           }
         ]
       }
-    }).then(this.displayResults(), console.error.bind(console));
-  }
-
-  
-  render(){
+    }).then(this.displayResults, console.error.bind(console));
+    }
+    const responseGoogleFailure = (error) => {
+      console.error(error);
+    };
+    const displayResults = (response) => {
+        console.log("aici");
+        var formattedJson = JSON.stringify(response.result, null, 2);
+        console.log(formattedJson);
+        document.getElementById('query-output').value = formattedJson;
+      }
     return (
         <body>
         <h1>Hello Analytics Reporting API V4</h1>
 
-        <p class="g-signin2" data-onsuccess="queryReports"></p>
+        <GoogleLogin
+              clientId="443094691967-2j7a99kuh7puj3dvb7m7f9i40j6lcjr3.apps.googleusercontent.com"
+              buttonText="Continuă cu GOOGLE"
+              onSuccess={queryReports}
+              onFailure={responseGoogleFailure}
+            />
 
         <textarea cols="80" rows="20" id="query-output"></textarea>
         </body>
