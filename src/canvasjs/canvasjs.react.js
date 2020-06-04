fc1b1e283686e3e25152820f56778a228eb6c9da
Bodin Roxana <bodinroxana@yahoo.com>
2020-06-04 19:12:25 +0300

    ga analytics reports

--- src/canvasjs/canvasjs.react.js
+++ src/canvasjs/canvasjs.react.js
@@ -9,7 +9,7 @@ class CanvasJSChart extends React.Component {
 		this.options = props.options ? props.options : {};		
 		this.containerProps = props.containerProps ? props.containerProps : {width: "100%", position: "relative"};
 		this.containerProps.height = props.containerProps && props.containerProps.height ? props.containerProps.height : this.options.height ? this.options.height + "px" : "400px";
-		this.chartContainerId = "canvasjs-react-chart-container-" //+ CanvasJSChart._cjsContainerId++;
+		this.chartContainerId = "canvasjs-react-chart-container-" + CanvasJSChart._cjsContainerId++;
 	}	
 	componentDidMount() {
 		//Create Chart and Render		
--- src/containers/Statistici.js
+++ src/containers/Statistici.js
@@ -4,25 +4,61 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import CanvasJSReact from '../canvasjs/canvasjs.react';
 import $ from 'jquery';
-import ChartViews from './ChartViews';
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+var CanvasJSChart1 = CanvasJSReact.CanvasJSChart;
 var VIEW_ID = '219697764';
-var event_category=[];
-var event_action=[];
-var event_count=[];
-var page_name=[];
-var page_views=[];
-var view_count=0;
-
+var event_category = [];
+var event_action = [];
+var event_count = [];
+var page_name = [];
+var page_views = [];
+var view_count = 0;
+//optiuni primul chart conectare
+var opt = {
+  animationEnabled: true,
+  exportEnabled: true,
+  theme: "dark2", // "light1", "dark1", "dark2"
+  title: {
+    text: "Modul de conectare al utilizatorilor"
+  },
+  data: [
+    {
+      type: "pie",
+      indexLabel: "{label}: {y}%",
+      startAngle: -90,
+      dataPoints: [
+      ]
+    }]
+};
+var opt2 = {
+  animationEnabled: true,
+  theme: "light2", // "light1", "light2", "dark1", "dark2"
+  title: {
+    text: "Topul profilurilor din ultimele 7 zile"
+  },
+  axisY: {
+    title: "Numărul de vizualizări",
+    includeZero: false
+  },
+  axisX: {
+    title: "Nume profil"
+  },
+  data: [{
+    type: "column",
+    dataPoints: [
+      // { label: "India", y: 7.1 },        
+    ]
+  }]
+}
 
 class Statistici extends Component {
   constructor(props) {
     super(props);
     this.queryReports = this.queryReports.bind(this);
-    this.state={
-      options:"",
-      options2:""
+    this.state = {
+      options1: "",
+      options2: ""
     }
   }
   queryReports() {
@@ -42,115 +78,85 @@ class Statistici extends Component {
             ],
             dimensions: [
               {
-                name:"ga:eventCategory"
+                name: "ga:eventCategory"
               },
               {
-                name:"ga:eventAction"
+                name: "ga:eventAction"
               }
             ],
             metrics: [
               {
-                expression:"ga:totalEvents",
-                formattingType:"INTEGER"
-             }
+                expression: "ga:totalEvents",
+                formattingType: "INTEGER"
+              }
             ]
           }
         ]
       }
-    }).then(function(response){
+    }).then(function (response) {
       console.log(response);
-      var formattedJson=response.result.reports[0].data.rows;
-      var count=0;
-      for(var i=0;i<formattedJson.length;i++)
-      {
+      var formattedJson = response.result.reports[0].data.rows;
+      //toate evenimentele
+      for (var i = 0; i < formattedJson.length; i++) {
         event_category.push(formattedJson[i].dimensions[0]);
         event_action.push(formattedJson[i].dimensions[1]);
-        event_count.push(formattedJson[i].metrics[0].values[0]); 
-        count=parseInt(count)+parseInt(formattedJson[i].metrics[0].values[0]);
-       
-       }
-      console.log(count);
-     var opt={
-      animationEnabled: true,
-      exportEnabled: true,
-      theme: "dark2", // "light1", "dark1", "dark2"
-      title:{
-        text: "Evenimente Conectare"
-      },
-      data: [{
-        type: "pie",
-        indexLabel: "{label}: {y}%",		
-        startAngle: -90,
-        dataPoints: [
-        ]
-      }]};
-      for(var i=0;i<event_category.length;i++)
-      {
-        if(event_category[i]=="Conectare")
-        {
-          opt.data[0].dataPoints.push({ y: Math.round(event_count[i]*100/count),label:event_action[i]});
+        event_count.push(formattedJson[i].metrics[0].values[0]);
+
+      }
+      //conectare login -cele 3 tipuri
+      var action = [];
+      var count_per_event = [];
+      var count = 0;
+
+      //separare chart-uri
+      for (var i = 0; i < event_category.length; i++) {
+        if (event_category[i] == "Conectare") {
+          action.push(event_action[i]); //descriere eveniment
+          count_per_event.push(event_count[i]); //numar
+          count = parseInt(count) + parseInt(event_count[i]); //total
         }
-        else if(event_category[i]=="Vizualizari")
-        {
+        else if (event_category[i] == "Vizualizari") {
           page_name.push(event_action[i]);
           page_views.push(event_count[i]);
-          view_count=parseInt(view_count)+parseInt(event_count[i]);
+          view_count = parseInt(view_count) + parseInt(event_count[i]);
         }
       }
-      this.setState((state, props) => ({
-        options:opt
-      }));
-      var opt2 = {
-        animationEnabled: true,
-        exportEnabled: true,
-        theme: "light2", //"light1", "dark1", "dark2"
-        title:{
-          text: "Topul profilurilor în funcție de numărul de vizualizări"
-        },
-        data: [{
-          type: "column", //change type to bar, line, area, pie, etc
-          //indexLabel: "{y}", //Shows y value on all Data Points
-          indexLabelFontColor: "#5A5757",
-          indexLabelPlacement: "outside",
-          dataPoints: [
-          ]
-        }]
-      };
-      page_views.sort();
-      console.log(page_views);
-      for(var i=0;i<page_views.length;i++)
-      {
-        if(i==0)
-          opt2.data[0].dataPoints.push({ x:page_name[i] ,y: Math.round(page_views[i]*100/view_count),indexLabel:"Cel mai vizualizat profil"});
-        else
-          opt2.data[0].dataPoints.push({ x:page_name[i] ,y: Math.round(page_views[i]*100/view_count)});
+      console.log(action,count_per_event,count);
+      //calculare si adaugare puncte pe chart pentru conectare
+      for (var i = 0; i < action.length; i++) {
+        opt.data[0].dataPoints.push({ y: Math.round(count_per_event[i] * 100 / count), label: action[i] });
       }
-      this.setState((state, props) => ({
-        options2:opt2
-      }));
-
-  }.bind(this));
- 
-   }
-  render(){
+      for (var i = 0; i < page_views.length; i++) {
+        if (i == 0)
+          opt2.data[0].dataPoints.push({ label: page_name[i], y: parseInt(page_views[i]) });
+        else
+          opt2.data[0].dataPoints.push({ label: page_name[i], y: parseInt(page_views[i]) });
+      }
+    //setare optiuni si desenare chart2
+    this.setState((state, props) => ({
+      options2: opt2,
+      options1: opt
+    }));
+    }.bind(this));
+  }
+  render() {
     const responseGoogleFailure = (error) => {
       console.error(error);
     };
-    
+
     return (
       <body>
         <h1>Rapoarte Google Analytics</h1>
         <GoogleLogin
-              clientId="443094691967-2j7a99kuh7puj3dvb7m7f9i40j6lcjr3.apps.googleusercontent.com"
-              buttonText="Continuă cu GOOGLE"
-              onSuccess={this.queryReports}
-              onFailure={responseGoogleFailure}
-            />
-         <CanvasJSChart options={this.state.options} ></CanvasJSChart> 
-         <ChartViews opti2={this.state.options2}></ChartViews>
-          </body>
+          clientId="443094691967-2j7a99kuh7puj3dvb7m7f9i40j6lcjr3.apps.googleusercontent.com"
+          buttonText="Continuă cu GOOGLE"
+          onSuccess={this.queryReports}
+          onFailure={responseGoogleFailure}
+        />
+        <CanvasJSChart options={this.state.options1} ></CanvasJSChart>
+        <CanvasJSChart1 options={this.state.options2}></CanvasJSChart1> 
+      </body>
     );
   }
 }
-//<textarea cols="80" rows="20" id="query-output"></textarea>
 export default Statistici;
